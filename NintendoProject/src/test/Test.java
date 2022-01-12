eb12e52f045adc7a6dded085f709370415f0600e
Antoine Rodriguez <rodriguez.antoine083@gmail.com>
2022-01-12 15:16:18 +0100

    Modifs Achat/client

--- NintendoProject/src/test/Test.java
+++ NintendoProject/src/test/Test.java
@@ -1,7 +1,11 @@
 package test;
 
-import model.Console;
-import model.Jeu;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import model.*;
 
 public class Test {
 
@@ -15,6 +19,16 @@ public class Test {
 		Jeu j4 = new Jeu("Street Fighter", c1);
 		Jeu j5 = new Jeu("Super Marios Bros", c2);
 		
+		Achat a1 = new Achat(j1,LocalDate.now(),30);
+		Achat a2 = new Achat(j5,LocalDate.now(),15.99);
+		
+		List<Achat> achats =new ArrayList<Achat>();
+		Collections.addAll(achats, a1, a2);
+		
+		Client c = new Client("Officier","Abid");
+		c.setAchats(achats);
+		
+		System.out.println(c);
 
 	}
 
