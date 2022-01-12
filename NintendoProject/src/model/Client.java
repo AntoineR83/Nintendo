5ca296405429b58d380001f12caea8836c9dc6ff
Vdellac <vdellac@outlook.fr>
2022-01-12 15:04:28 +0100

    Ajout objet Boutique + Client dans main

--- NintendoProject/src/model/Client.java
+++ NintendoProject/src/model/Client.java
@@ -4,7 +4,7 @@ public class Client {
 	private String nom;
 	private String prenom;
 	
-	Client (String nom , String prenom) {
+	public Client (String nom , String prenom) {
 		this.nom=nom;
 		this.prenom=prenom;
 	}
